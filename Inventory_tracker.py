d14abb97e9a39e358137996b46d139e2d675cb9a
DEBANJAN ROY CHOUDHURY <debanjanroychoudhury1@gmail.com>
2026-05-17 18:06:12 +0530

    Update Inventory_tracker.py

--- Inventory_tracker.py
+++ Inventory_tracker.py
@@ -1,19 +1,3 @@
-# print("Smart Inventory Tracker".center(40, " "))
-# print("Enter the Name of the Product (or type 'FINISH' to stop):")
-# print("")
-# print("The Items have logged Successfully ")
-# while True:
-#     item = input("Enter the Name of the Item(or type \'FINISH\' to stop): ")
-#     if item.lower() == 'finish':
-#         print("Inventory logging complete. Goodbye!")
-#         break
-#     print("Your item", item, "has been added in the chart")
-# while True:
-#     item = input("Enter the Name of the Item(or type \'FINISH\' to stop): ")
-#     print("Your item", item, "has been added in the chart")
-#     if item.lower() == 'finish':
-#         print("Inventory logging complete. Goodbye!")
-#         break
 print("Crash-Resistant Inventory Tracker".center(40," "))
 while True:
     item = input("Enter the Name of the Item (or type 'FINISH' to stop): ")
